7f3bf66a661d946d655c9403734679e05a40f6db
Martin Bee <martbee88@gmail.com>
2017-04-04 21:54:02 -0500

    Adds minfied react and react-dom.

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        Hi Hanna
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />,
